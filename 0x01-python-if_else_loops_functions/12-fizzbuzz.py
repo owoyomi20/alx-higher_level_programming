d6f839d2bccf4c810547c99e5513c24c125c8be3
owoyomi20 <owoyomitaiwoadeyemi@gmail.com>
2022-09-21 15:08:31 +0000

    python is cool

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for num in range(1, 101):
-        if num % 15 is 0:
+        if num % 15 == 0:
             print("FizzBuzz", end=" ")
-        elif num % 3 is 0:
+        elif num % 3 == 0:
             print("Fizz", end=" ")
-        elif num % 5 is 0:
+        elif num % 5 == 0:
             print("Buzz", end=" ")
         else:
             print(num, end=" ")
