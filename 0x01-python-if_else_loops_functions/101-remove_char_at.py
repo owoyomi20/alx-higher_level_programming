bc677b86ca3c03d83492f9e87562afc498f547e3
owoyomi20 <owoyomitaiwoadeyemi@gmail.com>
2022-09-21 14:20:38 +0000

    fix more bugs

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,4 +1,5 @@
-har_at(str, n):
+#!/usr/bin/python3
+def remove_char_at(str, n):
     new = ""
     i = 0
     for c in str:
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,4 @@
-aracter in range(97, 123):
+#!/usr/bin/python3
+for character in range(97, 123):
     if (character != 101 and character != 113):
         print("{:c}".format(character), end='')
